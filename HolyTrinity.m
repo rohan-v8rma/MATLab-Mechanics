0543cdca5708019a5fa121285c3ff61868a2de36
rohan-verma19 <rv725555@gmail.com>
2022-02-26 09:52:25 +0530

    feat: Added MATLab basic scripts

---
+++ HolyTrinity.m
@@ -0,0 +1,7 @@
+clc; %clears the commmand window
+clear; %clears all the workspace variables
+close all; %closes all windows
+
+% These commands can help reduce 
+% confusion when running multiple scripts
+% as well as organize your workspace
